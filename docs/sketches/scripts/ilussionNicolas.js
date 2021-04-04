4a837d48c2ebce70afcf301ecc59802e7006c42d
hnsaavedraa <hnsaavedraa@unal.edu.co>
2021-04-04 05:59:15 +0000

    Deploying to gh-pages from  @ b9c5ee5bc6d77d8ee3c9e629b928f5f6dc4e9f97 🚀

--- docs/sketches/scripts/ilussionNicolas.js
+++ docs/sketches/scripts/ilussionNicolas.js
@@ -1,10 +1,10 @@
-let opa = 255;
+// Definimos variables
+let exteriorColor = 255;
 let radio = 40;
 
 function setup() {
     var myCanvas = createCanvas(720, 560);
 }
-
 
 function draw() {
     background(255);
@@ -14,63 +14,68 @@ function draw() {
 function illusion() {
     push();
     translate((width / 4) + 50, height / 2);
-    uno();
+    figureOne();
     pop();
     push();
     translate((width / 4) * 3 + 50, height / 2);
-    dos();
+    figureTwo();
     pop();
 }
 
-function uno() {
-    center();
+// Posicionamos los circulos interiores e inferiores de la primer figura
+function figureOne() {
+    principalCircle();
     var an = (PI / 3);
     var i;
     for (i = 0; i < 6; i++) {
         push();
         translate(p5.Vector.fromAngle(i * an, 3.5 * radio));
-        exterior1();
+        exteriorCircleOne();
         pop();
     }
 }
 
-function dos() {
-    center();
+// Posicionamos los circulos interiores e inferiores de la segunda figura
+function figureTwo() {
+    principalCircle();
     var an = (2 * PI / 8);
     var i;
     for (i = 0; i < 8; i++) {
         push();
-        translate(p5.Vector.fromAngle(i * an, 2 * radio));
-        exterior2();
+        translate(p5.Vector.fromAngle(i * an, 1.8 * radio));
+        exteriorCircleTwo();
         pop();
     }
 }
-
-function center() {
+// Creacion de circulo interior
+function principalCircle() {
     ellipseMode(RADIUS);
     noStroke();
-    fill('yellow');
+    fill(34, 94, 201);
     ellipse(0, 0, radio, radio);
 }
 
-function exterior1() {
+// Creacion de cirulo exterior grande
+function exteriorCircleOne() {
     ellipseMode(RADIUS);
     noStroke();
-    fill(155, 0, 215, opa);
-    ellipse(0, 0, 1.5 * radio, 1.5 * radio);
+    fill(30, 178, 166, exteriorColor);
+    ellipse(0, 0, 1.6 * radio, 1.7 * radio);
 }
 
-function exterior2() {
+// Creacion de cirulo exterior pequeño
+function exteriorCircleTwo() {
     ellipseMode(RADIUS);
     noStroke();
-    fill(155, 0, 215, opa);
+    fill(30, 178, 166, exteriorColor);
     ellipse(0, 0, 0.5 * radio, 0.5 * radio);
 }
-
+// Definimos los eventos al opimir teclas, cambiaran el color de los circulos exteriores
+// igualandolo al del fondo
 function keyPressed() {
-    if (key === '0') {
-        opa = 255;
-    } else if (key === '1') {
-        opa = 0;
+    if (key === '1') {
+        exteriorColor = 255;
+    } else if (key === '2') {
+        exteriorColor = 0;
     }
 }
