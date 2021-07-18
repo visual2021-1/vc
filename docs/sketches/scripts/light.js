9650b5cd5fc33ff78a3ca284afb4fca512d9ff47
FernandoMorantes <43284808+FernandoMorantes@users.noreply.github.com>
2021-07-18 23:56:56 +0000

    Deploying to gh-pages from  @ 990b07ea4ce15f5c09ec9724e07a139884373262 🚀

--- docs/sketches/scripts/light.js
+++ docs/sketches/scripts/light.js
@@ -1,44 +1,140 @@
-function setup() {
-    createCanvas(700, 560, WEBGL);
-    setAttributes('antialias', true);
-    fill(237, 34, 93);
-    strokeWeight(0);
-}
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+import {
+    OrbitControls
+} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
+import {
+    GUI
+} from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
 
-function draw() {
-    background(200);
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({
+        canvas
+    });
 
-    let locX = mouseX - height / 2;
-    let locY = mouseY - width / 2;
+    const fov = 45;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
 
-    ambientLight(60, 60, 60);
-    pointLight(255, 255, 255, locX, locY, 100);
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
 
-    //ambientMaterial(250);
-    specularMaterial(123);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    //rotateZ(frameCount * 0.01);
-    lights();
-    apple();
-}
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
 
+    {
+        const planeSize = 40;
 
-function apple() {
-    size = 20;
-    increment = 0.1;
-    for (let u = 0; u < TWO_PI; u += increment) {
-        beginShape(TRIANGLE_STRIP);
-        for (let v = -PI; v < PI; v += increment) {
-            x = cos(u) * (4 + 3.8 * cos(v))
-            y = sin(u) * (4 + 3.8 * cos(v))
-            z = (cos(v) + sin(v) - 1) * (1 + sin(v)) * log(1 - PI * v / 10) + 7.5 * sin(v)
-            vertex(size * x, size * y, size * z);
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        texture.repeat.set(repeats, repeats);
 
-            x = cos(u + 0.1) * (4 + 3.8 * cos(v))
-            y = sin(u + 0.1) * (4 + 3.8 * cos(v))
-            vertex(size * x, size * y, size * z);
-        }
-        endShape(CLOSE);
+        const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+        });
+        const mesh = new THREE.Mesh(planeGeo, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        scene.add(mesh);
+    } {
+        const cubeSize = 4;
+        const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+        const cubeMat = new THREE.MeshPhongMaterial({
+            color: '#8AC'
+        });
+        const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+        mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+        scene.add(mesh);
+    } {
+        const sphereRadius = 3;
+        const sphereWidthDivisions = 32;
+        const sphereHeightDivisions = 16;
+        const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+        const sphereMat = new THREE.MeshPhongMaterial({
+            color: '#CA8'
+        });
+        const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+        mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+        scene.add(mesh);
     }
+
+    class ColorGUIHelper {
+        constructor(object, prop) {
+            this.object = object;
+            this.prop = prop;
+        }
+        get value() {
+            return `#${this.object[this.prop].getHexString()}`;
+        }
+        set value(hexString) {
+            this.object[this.prop].set(hexString);
+        }
+    }
+
+    function makeXYZGUI(gui, vector3, name, onChangeFn) {
+        const folder = gui.addFolder(name);
+        folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+        folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+        folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+        folder.open();
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.PointLight(color, intensity);
+        light.position.set(0, 10, 0);
+        scene.add(light);
+
+        const helper = new THREE.PointLightHelper(light);
+        scene.add(helper);
+
+        function updateLight() {
+            helper.update();
+        }
+
+        const gui = new GUI();
+        gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+        gui.add(light, 'intensity', 0, 2, 0.01);
+        gui.add(light, 'distance', 0, 40).onChange(updateLight);
+
+        makeXYZGUI(gui, light.position, 'position');
+    }
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render() {
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
+
+main();
